708ed1563bb7ccafab539a47c7a5181e20c2bbfd
moobarrac <wahabajibola01@gmail.com>
2023-04-18 14:45:59 +0100

    added client side routing

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import About from './pages/About';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
